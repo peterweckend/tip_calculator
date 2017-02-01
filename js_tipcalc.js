58c4fffb2108c92e12a14b84d6ebac3d1a9e0818
Peter Weckend <PeterWeckend@Peters-MBP.hitronhub.home>
2017-01-31 19:40:39 -0700

    minor fixes

--- js_tipcalc.js
+++ js_tipcalc.js
@@ -1,4 +1,4 @@
-// window.alert()
+
 
 function CalculateValues(){
 	var finalamount = Total();
@@ -7,7 +7,7 @@ function CalculateValues(){
 		TotalOutput(finalamount);
 		TipOutput(tipamount);
 
-		if ($('#peoplecount').val() != '1') {
+		if ($('#peoplecount').val() != '1') {	// split bill if > 1 person
 			$('.group3').css("display", "table-row");
 			$('.group1').css("display", "table-cell");
 			$('.group2').css("display", "table-cell");
@@ -24,13 +24,13 @@ function CalculateValues(){
 
 }
 
-function Total(){
+function Total(){	// calculates value of subtotal + tip
 	var subtotal = Number($('#subtotal').val().slice(1));
 	var tipval = Number($('#tipval').val().slice(0,-1)) / 100;
 	return subtotal + (subtotal * tipval);
 }
 
-function TipTotal(){
+function TipTotal(){	// calculates the value of the tip only
 	var subtotal = Number($('#subtotal').val().slice(1));
 	var tipval = Number($('#tipval').val().slice(0,-1)) / 100;
 	return subtotal * tipval;
@@ -38,10 +38,10 @@ function TipTotal(){
 
 
 // mode = 1 is final check, we can ignore $ and %
+// make sure tip is a valid value
 function ValidateTip(mode){
 
 	var valid = 1;
-
 	if (mode == 1){
 		var tipamt1 = Number($('#tipval').val().slice(0, -1));
 
@@ -74,6 +74,7 @@ function ValidateTip(mode){
 }
 
 // mode = 1 is final check, we can ignore $ and %
+// make sure subtotal is a valid value
 function ValidateSub(mode){
 
 	var valid = 1;
@@ -108,14 +109,14 @@ function ValidateSub(mode){
 }
 
 
-
-function ValidateValues(){
-	var province = $('#province').val();
-	if (province == "default"){
-		return 0;
-	}
-	return 1;
-}
+// was originally used to calculate tax for provinces
+// function ValidateValues(){
+// 	var province = $('#province').val();
+// 	if (province == "default"){
+// 		return 0;
+// 	}
+// 	return 1;
+// }
 
 function TipOutput(output){
 	$('#tipamt').val('$'+output.toFixed(2));
@@ -125,19 +126,22 @@ function TotalOutput(output){
 	$('#total_sol').val('$'+output.toFixed(2));
 }
 
+// for split groups
 function GroupOutput(split_tip, split_total){
 	$('#split_tipamt').val('$'+split_tip.toFixed(2));
 	$('#split_total_sol').val('$'+split_total.toFixed(2));
 }
 
+// if invalid input entered
 function TipError(){
 	$('#tipamt').val('$0.00');
 }
-
+// if invalid input entered
 function TotalError(){
 	$('#total_sol').val('$0.00');
 }
 
+// on Reset button press
 function Reset(){
 	$('#tipamt').val('$0.00');
 	$('#total_sol').val('$0.00');
@@ -147,12 +151,14 @@ function Reset(){
 	$('#peoplecount').val('1');
 }
 
+// if valid input, display percentage sign
 function AddPercent(){
 	if (ValidateTip(0)){
 		$('#tipval').val($('#tipval').val()+'%');
 	} 
 }
 
+// if valid input, display dollar sign
 function AddDollar(){
 	if (ValidateSub(0)){
 		$('#subtotal').val('$'+$('#subtotal').val());
